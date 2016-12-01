96108fe7078f1821c5f9d1407689f4904998bc80
Wylliam Judd <wylliamjudd@gmail.com>
2016-12-01 15:55:02 -0800

    configure generators

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Social
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine :jbuilder
+      g.test_framework nil
+      g.assets false
+      g.helper false
+    end
   end
 end
